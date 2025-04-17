a1684aa73cd8d1df759a4e59eaede91ca7148694
stphnVi <stephanie1117@estudiantec.cr>
2025-04-17 00:41:32 -0600

    [add] add buttons & doors with proper message

--- MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/CasaModelo.java
+++ MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/CasaModelo.java
@@ -1,29 +1,28 @@
 package com.example.miprimeraplicacion;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.core.content.ContextCompat;
-import android.graphics.drawable.GradientDrawable;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class CasaModelo extends AppCompatActivity {
 
     private boolean pantallaCasaModeloAbierta;
-
-    private boolean cuarto_encendido = false;
-    private boolean baño_encendido = false;
+    private boolean cuartoC1_encendido = false;
+    private boolean cuartoC2_encendido = false;
     private boolean sala_encendido = false;
-
-    private boolean pgarage_abierta = false;
+    private boolean cocina_encendido = false;
+    private boolean puertaP_abierta = false;
+    private boolean puertaC1_abierta = false;
+    private boolean puertaC2_abierta = false;
+    private boolean puerta_cocina_abierta = false;
+    private boolean puerta_principal_abierta = false;
 
     @Override
     public void onBackPressed() {
@@ -44,70 +43,152 @@ public class CasaModelo extends AppCompatActivity {
         pantallaCasaModeloAbierta = true;
 
 
+        Button botonluzC1 = findViewById(R.id.LuzC1);
+        Button botonluzC2 = findViewById(R.id.LuzC2);
+        Button botonluzCocina = findViewById(R.id.LuzCocina);
+        Button botonluzSala = findViewById(R.id.LuzSala);
 
-        Button botonluzcuarto = findViewById(R.id.cuarto);
-        Button botonluzbaño = findViewById(R.id.baño);
-        Button botonluzsala = findViewById(R.id.sala);
-        Button botonpgarage = findViewById(R.id.pgarage);
+        Button botonPuertaP = findViewById(R.id.PuertaPatio);
+        Button botonPuertaC1 = findViewById(R.id.PuertaC1);
+        Button botonPuertaC2 = findViewById(R.id.PuertaC2);
+        Button botonPuertaCocina = findViewById(R.id.PuertaCocina);
+        Button botonPuertaPrin = findViewById(R.id.PuertaPrincipal);
+
         Button botonregresarprincipal = findViewById(R.id.regresarprincipal);
 
-        botonluzcuarto.setOnClickListener(view -> {
+        GradientDrawable BotonSalafondo = (GradientDrawable) botonluzSala.getBackground();
+        GradientDrawable BotonCocinafondo = (GradientDrawable) botonluzCocina.getBackground();
+        GradientDrawable BotonC1fondo = (GradientDrawable) botonluzC1.getBackground();
+        GradientDrawable BotonC2fondo = (GradientDrawable) botonluzC2.getBackground();
 
-            if(!cuarto_encendido) {
-                botonluzcuarto.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
-            }else{
-                botonluzcuarto.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
-
-            }
-            String messagecuarto = cuarto_encendido ? "LED2_OFF" : "LED2_ON";
-            String messageSendcuarto = "func: luzcuarto" + messagecuarto;
-            Socket.sendMessage(messageSendcuarto);
-            cuarto_encendido = !cuarto_encendido;
-        });
-
-        botonluzbaño.setOnClickListener(view -> {
-
-            if(!baño_encendido) {
-                botonluzbaño.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
-            }else{
-                botonluzbaño.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
-
-            }
-
-            String messagebaño = baño_encendido ? "LED3_OFF" : "LED3_ON";
-            String messageSendbaño = "func: luzbaño" + messagebaño;
-            Socket.sendMessage(messageSendbaño);
-            baño_encendido = !baño_encendido;
-        });
-
-        botonluzsala.setOnClickListener(view -> {
+        botonluzSala.setOnClickListener(view -> {
 
             if(!sala_encendido) {
-                botonluzsala.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
-            }else{
-                botonluzsala.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
-
+                BotonSalafondo.setColor(ContextCompat.getColor(this, R.color.encendido));
+            } else {
+                BotonSalafondo.setColor(ContextCompat.getColor(this, R.color.apagado));
             }
-
-            String messagesala = sala_encendido ? "LED1_OFF" : "LED1_ON";
-            String messageSendsala = "func: luzsala" + messagesala;
-            Socket.sendMessage(messageSendsala);
+            String messagecuarto = sala_encendido ? "LED2_OFF" : "LED2_ON";
+            String messageSendcuarto = "func: LuzSala" + messagecuarto;
+            Socket.sendMessage(messageSendcuarto);
             sala_encendido = !sala_encendido;
         });
 
-        botonpgarage.setOnClickListener(view -> {
+        botonluzCocina.setOnClickListener(view -> {
 
-            if(!pgarage_abierta) {
-                botonpgarage.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
+            if(!cocina_encendido) {
+                BotonCocinafondo.setColor(ContextCompat.getColor(this, R.color.encendido));
+            } else {
+                BotonCocinafondo.setColor(ContextCompat.getColor(this, R.color.apagado));
+            }
+            String messagecuarto = cocina_encendido ? "LED2_OFF" : "LED2_ON";
+            String messageSendcuarto = "func: luzCocina" + messagecuarto;
+            Socket.sendMessage(messageSendcuarto);
+            cocina_encendido = !cocina_encendido;
+        });
+
+
+        botonluzC1.setOnClickListener(view -> {
+
+            if(!cuartoC1_encendido) {
+                BotonC1fondo.setColor(ContextCompat.getColor(this, R.color.encendido));
+            } else {
+                BotonC1fondo.setColor(ContextCompat.getColor(this, R.color.apagado));
+            }
+            String messagecuarto = cuartoC1_encendido ? "LED2_OFF" : "LED2_ON";
+            String messageSendcuarto = "func: LuzC1" + messagecuarto;
+            Socket.sendMessage(messageSendcuarto);
+            cuartoC1_encendido = !cuartoC1_encendido;
+        });
+
+        botonluzC2.setOnClickListener(view -> {
+
+            if(!cuartoC2_encendido) {
+                BotonC2fondo.setColor(ContextCompat.getColor(this, R.color.encendido));
             }else{
-                botonpgarage.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
+                BotonC2fondo.setColor(ContextCompat.getColor(this, R.color.apagado));
 
             }
 
-            String messagepgarage = pgarage_abierta ? "motor_CLOSE" : "motor_OPEN";
-            String messageSendpgarage = "func: " + messagepgarage;
+            String messagebaño = cuartoC2_encendido ? "LED3_OFF" : "LED3_ON";
+            String messageSendbaño = "func: LuzC2" + messagebaño;
+            Socket.sendMessage(messageSendbaño);
+            cuartoC2_encendido = !cuartoC2_encendido;
+        });
+
+        botonPuertaP.setOnClickListener(view -> {
+
+            if(!puertaP_abierta) {
+                botonPuertaP.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
+            }else{
+                botonPuertaP.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
+
+            }
+
+            String messagesala = puertaP_abierta ?  "motor_CLOSE" : "motor_OPEN";
+            String messageSendsala = "func: Patio" + messagesala;
+            Socket.sendMessage(messageSendsala);
+            puertaP_abierta = !puertaP_abierta;
+        });
+
+        botonPuertaPrin.setOnClickListener(view -> {
+
+            if(!puerta_principal_abierta) {
+                botonPuertaPrin.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
+            }else{
+                botonPuertaPrin.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
+
+            }
+
+            String messagesala = puerta_principal_abierta ?  "motor_CLOSE" : "motor_OPEN";
+            String messageSendsala = "func: Principal" + messagesala;
+            Socket.sendMessage(messageSendsala);
+            puerta_principal_abierta = !puerta_principal_abierta;
+        });
+
+        botonPuertaC1.setOnClickListener(view -> {
+
+            if(!puertaC1_abierta) {
+                botonPuertaC1.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
+            }else{
+                botonPuertaC1.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
+
+            }
+
+            String messagepgarage = puertaC1_abierta ? "motor_CLOSE" : "motor_OPEN";
+            String messageSendpgarage = "func: PuertaC1" + messagepgarage;
             Socket.sendMessage(messageSendpgarage);
-            pgarage_abierta = !pgarage_abierta;
+            puertaC1_abierta = !puertaC1_abierta;
+        });
+
+        botonPuertaC2.setOnClickListener(view -> {
+
+            if(!puertaC2_abierta) {
+                botonPuertaC2.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
+            }else{
+                botonPuertaC2.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
+
+            }
+
+            String messagepgarage = puertaC2_abierta ? "motor_CLOSE" : "motor_OPEN";
+            String messageSendpgarage = "func: PuertaC2" + messagepgarage;
+            Socket.sendMessage(messageSendpgarage);
+            puertaC2_abierta = !puertaC2_abierta;
+        });
+
+        botonPuertaCocina.setOnClickListener(view -> {
+
+            if(!puerta_cocina_abierta) {
+                botonPuertaCocina.setBackgroundColor(ContextCompat.getColor(this, R.color.encendido));
+            }else{
+                botonPuertaCocina.setBackgroundColor(ContextCompat.getColor(this, R.color.apagado));
+
+            }
+
+            String messagepgarage = puerta_cocina_abierta ? "motor_CLOSE" : "motor_OPEN";
+            String messageSendpgarage = "func: PuertaCocina" + messagepgarage;
+            Socket.sendMessage(messageSendpgarage);
+            puerta_cocina_abierta = !puerta_cocina_abierta;
         });
 
 
